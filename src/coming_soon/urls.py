b05a38290f8798d367d2ec3aa6855a7102eadb62
Thapelo <me@tsotetsithapelo.co.za>
2017-07-05 10:53:37 +0200

    Fix url.

--- src/coming_soon/urls.py
+++ src/coming_soon/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.ContactUsView, name='coming-soon'),
+    url(r'^$', views.ContactUsView.as_view(), name='coming-soon'),
 ]

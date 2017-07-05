19893b49268fcbd420558292ef44eff3ae5674c5
Thapelo <me@tsotetsithapelo.co.za>
2017-07-05 11:27:40 +0200

    Add users to the db.

--- src/coming_soon/views.py
+++ src/coming_soon/views.py
@@ -1,6 +1,8 @@
 from .forms import ContactUsForm
 from django.views.generic import FormView
 
+from .models import PrelaunchSignUp
+
 
 class ContactUsView(FormView):
     template_name = 'contact.html'
@@ -9,3 +11,15 @@ class ContactUsView(FormView):
 
     def form_valid(self, form):
         return super(ContactUsView, self).form_valid(form)
+
+    @staticmethod
+    def create_user_details(name, email):
+        PrelaunchSignUp.objects.create(name=name, email=email)
+
+    def post(self, request, *args, **kwargs):
+        data = request.POST
+        name = data['name']
+        email = data['email']
+
+        self.create_user_details(name, email)
+        return super(ContactUsView, self).post(request)

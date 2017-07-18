421a3077ec2e36c50f3b010050066b81311f4e77
Thapelo <me@tsotetsithapelo.co.za>
2017-07-18 16:26:34 +0200

    Update tests.

--- src/coming_soon/tests/unit/test_contact_us_view.py
+++ src/coming_soon/tests/unit/test_contact_us_view.py
@@ -1,11 +1,10 @@
 from unittest import TestCase
 
-from django.test import RequestFactory
+from django.test import Client
 from django.urls import reverse
-from django.contrib.messages.storage.fallback import FallbackStorage
 from django.core import mail
 
-from coming_soon.views import ContactUsView
+from coming_soon.forms import ContactUsForm
 
 
 class ContactUsViewTest(TestCase):
@@ -14,39 +13,40 @@ class ContactUsViewTest(TestCase):
             'name': 'John Doe',
             'email': 'fe@example.com'
         }
-        self.factory = RequestFactory()
+        self.client = Client()
 
-    def create_view_request(self):
-        request = self.factory.get(reverse('coming-soon'))
-        response = ContactUsView.as_view()(request)
-        return response
+    def create_view_get_request(self):
+        return self.client.get(reverse('coming-soon'))
 
-    def create_view_post_request(self):
-        request = self.factory.post(reverse('coming-soon'), data=self.user_data)
-        setattr(request, 'session', 'session')
-        messages = FallbackStorage(request)
-        setattr(request, '_messages', messages)
-        response = ContactUsView.as_view()(request)
-        return response
+    def create_view_post_request(self, data=None):
+        return self.client.post(reverse('coming-soon'), data=data)
 
     def test_coming_soon_get(self):
-        response = self.create_view_request()
+        response = self.create_view_get_request()
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], 'contact.html')
 
     def test_coming_soon_post(self):
-        response = self.create_view_post_request()
-        self.assertEqual(response.status_code, 200)
+        response = self.create_view_post_request(data=self.user_data)
+        self.assertEqual(response.status_code, 302)
 
-    def test_send_email(self):
-        response = self.create_view_post_request()
-        import pdb;pdb.set_trace()
+        # Check that an email was sent to the user.
+        self.assertEqual(len(mail.outbox), 1)
 
-    def test_create_user(self):
-        response = self.create_view_request()
-        self.assertEqual(response.status_code, 200)
-        # TODO: Test create user.
+        # Check that post redirects to same url.
+        self.assertEqual(response.url, reverse('coming-soon'))
 
-    def test_post(self):
-        response = self.create_view_request()
-        self.assertEqual(response.status_code, 200)
-        # TODO: Test post request.
+    def test_coming_soon_post_form(self):
+        invalid_form_data = {
+            'name': '',
+            'email': 'test'
+        }
+        form = ContactUsForm(data=invalid_form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors, {'email': ['Enter a valid email address.'], 'name': ['This field is required.']})
+        valid_form_data = {
+            'name': 'John Doe',
+            'email': 'cs@example.com'
+        }
+        form = ContactUsForm(data=valid_form_data)
+        self.assertTrue(form.is_valid())
